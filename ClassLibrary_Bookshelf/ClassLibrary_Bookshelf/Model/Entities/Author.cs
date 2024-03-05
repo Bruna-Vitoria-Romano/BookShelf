d28181b591ea7675de97f47947a917ce48ba5e55
Bruna Romano <brunaclaudia468@gmail.com>
2024-03-04 22:10:14 -0300

    commit inicial

---
+++ ClassLibrary_Bookshelf/ClassLibrary_Bookshelf/Model/Entities/Author.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_Bookshelf.Model.Entities
+{
+    public class Author
+    {
+        public Author(int id, string name, int age, Book publishedBooks)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            PublishedBooks = publishedBooks;
+        }
+
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private int Age { get; set; }
+        private Book PublishedBooks { get; set; }
+
+        // Link study https://github.com/marciocrodrigues/Teste-Empresa-.net-core/blob/master/TesteBitzen/TesteBitzen.DOMAIN/Entities/Usuario.cs
+    }
+}
---
+++ ClassLibrary_Bookshelf/ClassLibrary_Bookshelf/Model/Entities/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_Bookshelf.Model.Entities
+{
+    public class Book
+    {
+        public Book(int id, string tittle, string literaryGenre, Author idAuthor, int ageRating, string description)
+        {
+            Id = id;
+            Tittle = tittle;
+            LiteraryGenre = literaryGenre;
+            IdAuthor = idAuthor;
+            AgeRating = ageRating;
+            Description = description;
+        }
+
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+        public string LiteraryGenre { get; set; }
+        public Author IdAuthor { get; set; }
+        public int AgeRating { get; set; }
+        public string Description { get; set; }
+    }
+}
